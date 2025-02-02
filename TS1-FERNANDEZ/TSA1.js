f95d8ce790294e15ac960750334315773a0d1a65
godseraph18 <fernandezelijahcarl@gmail.com>
2025-02-03 00:32:23 +0800

    Working

---
+++ TS1-FERNANDEZ/TSA1.js
@@ -0,0 +1,13 @@
+let colorToggle = false;
+
+function changeColor() {
+    const resume = document.getElementById('resume');
+    if (colorToggle) {
+        resume.style.backgroundColor = 'white';
+        document.body.style.backgroundColor = '#f4f4f4';
+    } else {
+        resume.style.backgroundColor = '#e0f7fa';
+        document.body.style.backgroundColor = '#b2ebf2';
+    }
+    colorToggle = !colorToggle;
+}
